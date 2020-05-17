f8e695255603757c0e2ae592b9cf698d6ec5bf6b
chenglong.wang <tiny.wcl@gmail.com>
2020-05-17 21:57:42 +0800

    concurrent, AtomicPtr and raw pointer

--- src/concurrent/atomics.rs
+++ src/concurrent/atomics.rs
@@ -1,21 +1,34 @@
 use std::sync::atomic::Ordering::SeqCst;
-use std::sync::atomic::{AtomicBool, AtomicI32};
+use std::sync::atomic::{AtomicBool, AtomicI32, AtomicPtr};
 use std::thread;
+use std::ptr::{null, null_mut};
 
 static BOOL_X: AtomicBool = AtomicBool::new(false);
 static BOOL_Y: AtomicBool = AtomicBool::new(false);
 static NUM_Z: AtomicI32 = AtomicI32::new(0);
 
+static mut NUM_PRT: AtomicPtr<i32> = AtomicPtr::new(0 as *mut i32);
+
 pub fn proc_atomics() {
+    unsafe {
+        println!(":#? {:#?}", NUM_PRT);
+        NUM_PRT = AtomicPtr::new(&mut 2);
+        println!(":? {:?}, {:?}", NUM_PRT, *NUM_PRT.get_mut());
+        *NUM_PRT.get_mut() = 5 as *mut i32;
+        println!("#?: {:#?}", NUM_PRT);
+        println!("AtomicPtr deref: {}", *NUM_PRT.get_mut() as i32);
+
+    }
+
     let t_x = thread::spawn(move || write_x());
     let t_y = thread::spawn(move || write_y());
     let t_x_y = thread::spawn(move || read_x_then_y());
     let t_y_x = thread::spawn(move || read_y_then_x());
 
-    t_x.join();
-    t_y.join();
-    t_x_y.join();
-    t_y_x.join();
+    let _ = t_x.join();
+    let _ = t_y.join();
+    let _ = t_x_y.join();
+    let _ = t_y_x.join();
     println!("x: {:?}, y: {:?}, z: {:?}", BOOL_X, BOOL_Y, NUM_Z)
 }
 
